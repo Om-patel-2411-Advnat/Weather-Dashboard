610bad2291107e9fa3586838873fd91abbc3feb9
Om Patel <om@advant.xyz>
2026-03-30 17:03:48 +0530

    fixed : the fetching url

--- utils/fetchWeatherData.js
+++ utils/fetchWeatherData.js
@@ -1,5 +1,5 @@
 export default async function fetchWeatherData(location){
-    const weatherRes = await fetch(`http://localhost:3000/api/weather?lat=${location.latitude}&lon=${location.longitude}`);
+    const weatherRes = await fetch(`/api/weather?lat=${location.latitude}&lon=${location.longitude}`);
 
     const weather = await weatherRes.json();
 
